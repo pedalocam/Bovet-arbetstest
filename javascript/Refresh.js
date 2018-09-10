72b4d01bcc5a60d29876b59762770a206a5a79f3
pedalocam <37455273+pedalocam@users.noreply.github.com>
2018-09-11 00:44:07 +0200

    Update Refresh.js

--- javascript/Refresh.js
+++ javascript/Refresh.js
@@ -21,8 +21,8 @@ function loadNewData() {
   count = 0;
   $.getJSON('https://api.unsplash.com/photos/random/?client_id=3db5ef77cc15f221119e4d4bde44a68b4cc1d1a36ba0a1955b5d120a37c94c58', {
     count: numItemsToGenerate,
-    w: 270,
-    h: 210
+    w: 200,
+    h: 160
   }, function (data) {
     count = 0;
     $.each($("img"), function () {
